98cbf43a8b212acbc6a1019911bfc50d899893c5
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-04-03 17:39:45 +0200

    add databasse functions

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
-const express = require("express")
+const express = require("express");
+const logger = require("../logging/logger");
+const db = require("./database/db_functions");
 
 const app = express();
 const port = 3000;
 
+
 // endpoints
 // index /
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+app.get("/", async (req, res) => {
+  logger.log("info", `HTTP ${res.statusCode} ${req.method} ${req.url}`);
+  let rows = await db.getAllTechs();
+  res.status(200).json(rows)
+  // do stuff with rows here
 });
 
 app.listen(port, () => console.log(`Homepage REST API listening on port ${port}`))
---
+++ backend/database/db_functions.js
@@ -0,0 +1,127 @@
+const db = require("./database.js")
+const logger = require("../../logging/logger")
+
+
+// returns all projects
+const getAllProjects = async () => {
+  let conn;
+  try {
+    conn = await db.pool.getConnection();
+    const rows = await conn.query(`
+        SELECT image_path, title, description, JSON_ARRAYAGG(name) AS techsUsed, link
+        FROM project
+        NATURAL JOIN projectUsesTech
+        NATURAL JOIN technology
+        GROUP BY projectID`);
+    return rows;
+  } catch (err) {
+    logger.log("error", err);
+    return 500;
+  } finally {
+    if (conn) conn.end();
+  }
+};
+
+// returns all technologies
+const getAllTechs = async () => {
+  let conn;
+  try {
+    conn = await db.pool.getConnection();
+    const rows = await conn.query(
+      `SELECT * FROM technology`
+    );
+    return rows;
+  } catch (err) {
+    logger.log("error", err);
+    return 500;
+  } finally {
+    if (conn) conn.end();
+  }
+};
+
+// insert a new technology
+const newTech = async (technology) => {
+  let conn;
+  try {
+    conn = await db.pool.getConnection();
+    const res = await conn.query(`INSERT INTO technology (name) VALUES (?)`, [technology.name]);
+    if (res["affectedRows"] === 1) 
+      return 201;
+    else return 500;
+  } catch (err) {
+    logger.log("error", err);
+    return 500;
+  } finally {
+    if (conn) conn.end();
+  }
+}
+
+// adds a new project
+const newProject = async (project, image) => {
+  let conn;
+  try {
+    conn = await db.pool.getConnection();
+    await conn.beginTransaction();
+    let res = await conn.query(
+      `
+      INSERT INTO project (title, description, link, image_path)
+      VALUES (?, ?, ?, ?)
+      RETURNING projectID
+      `, [project.title, project.description, project.link, image]
+    );
+    const projectID = res["projectID"];
+    let q = "";
+    project.techsUsed.forEach(tech => {
+      q += `(${projectID}, ${tech.techID})`
+    });
+    q = q.slice(0, -1);
+    res = await conn.query(`
+      INSERT INTO projectUsesTech (projectID, techID)
+      VALUES ?`, [q]
+    );
+    return 201;
+  } catch (err) {
+    logger.log("error", err);
+    return 500;
+  } finally {
+    if (conn) conn.end();
+  }
+};
+
+// returns all recepies, (just name and ID)
+const getAllRecepies = async () => {
+  let conn;
+  try {
+    conn = await db.pool.getConnection();
+    let rows = await conn.query(`SELECT recipeID, json_value(recipeJson, '$.name') AS recipeName FROM recipe`);
+    return rows;
+  } catch (err) {
+    logger.log("error", err);
+    return 500;
+  } finally {
+    if (conn) conn.end();
+  }
+};
+
+const getRecepie = async (id) => {
+  let conn;
+  try {
+    conn = await db.pool.getConnection();
+    let rows = await conn.query(`SELECT * FROM recipe WHERE recipeID = ?`, [id]);
+    return rows;
+  } catch (err) {
+    logger.log("error", err);
+    return 404;
+  } finally {
+    if (conn) conn.end();
+  }
+};
+
+module.exports = {
+  getAllProjects,
+  newProject,
+  getAllRecepies,
+  getRecepie,
+  getAllTechs,
+  newTech
+};
