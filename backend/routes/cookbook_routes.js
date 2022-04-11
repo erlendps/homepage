cb9167c9bc96cc0448c9e0881efb39104c78e403
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-04-11 15:58:14 +0200

    add multer config

--- backend/routes/cookbook_routes.js
+++ backend/routes/cookbook_routes.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+
 const logger = require("../../logging/logger");
 const db = require("../database/db_functions");
+
+
+// multer config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./static/cookbook/");
+  }, 
+  filename: (req, file, cb) => {
+    let recipeName = req.body["title"].replace(/\s/g, '');
+    let filename = recipeName + path.extname(file.originalname);
+    cb(null, filename);
+  }
+});
+
+const upload = multer({storage: storage});
 
 module.exports = router;
