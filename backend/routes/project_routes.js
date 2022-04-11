f163c64a7c9ee2be1949902e05f7de8c757a2c91
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-04-11 15:57:59 +0200

    finish project routes

--- backend/routes/project_routes.js
+++ backend/routes/project_routes.js
@@ -46,7 +46,41 @@ router.get("/newproject", async (req, res) => {
 
 router.post("/newproject", upload.single("project_image"), async (req, res) => {
   let body = req.body();
+  logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/projects${req.url}`);
+  if (!req.file) {
+    return res.status(500).send("<h1>500 Internal Server Error</h1><p>Something is wrong on our end, sorry!</p>")
+  }
+  let img_src;
+  if (process.env.NODE_ENV === "dev") {
+    img_src = "http://localhost:3001/static/projects/" + req.file.filename;
+  } else {
+    img_src = "https://pauska.no/static/projects/" + req.file.filename;
+  }
+  const result = await db.newProject(body, img_src);
+  if (result === 201) {
+    return res.status(201).send("Sucessfully created project");
+  }
+  return res.status(500).send("<h1>500 Internal Server Error</h1><p>Something is wrong on our end, sorry!</p>")
 });
+
+router.get("/newtech", async (req, res) => {
+  const result = await db.getAllTechs();
+  logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/projects${req.url}`);
+
+  if (result === 500) {
+    return res.status(500).send("<h1>500 Internal Server Error</h1><p>Something is wrong on our end, sorry!</p>")
+  }
+  res.status(200).json(result);
+});
+
+router.post("/newtech", async (req, res) => {
+  const body = req.body();
+  const result = await db.newTech(body);
+  if (result === 201) {
+    return res.status(201).send("Technology created successfully")
+  }
+  return res.status(500).send("<h1>500 Internal Server Error</h1><p>Something is wrong on our end, sorry!</p>");
+})
 
 
 module.exports = router;
