555ad1ba25d7b7539cba1c28715c238690acd913
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-06-19 13:20:13 +0200

    add cookbook routes

--- backend/routes/admin_routes.js
+++ backend/routes/admin_routes.js
@@ -15,7 +15,11 @@ const isAdmin = async (req, res, next) => {
   }
 }
 
+/**********************/
+
 // multer config
+
+/**********************/
 const cookbook_storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./static/cookbook/");
@@ -29,7 +33,6 @@ const cookbook_storage = multer.diskStorage({
 
 const cookbook_upload = multer({storage: cookbook_storage});
 
-// multer config
 const project_storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./static/projects/");
@@ -44,6 +47,11 @@ const project_storage = multer.diskStorage({
 const project_upload = multer({storage: project_storage});
 
 
+/**********************/
+
+// project routes
+
+/**********************/
 router.get("/newproject", async (req, res) => {
   const result = await db.getAllTechs();
   logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
@@ -74,6 +82,12 @@ router.post("/newproject", project_upload.single("project_image"), async (req, r
   return res.status(500).send("<h1>500 Internal Server Error</h1><p>Something is wrong on our end, sorry!</p>")
 });
 
+/**********************/
+
+// technology routes
+
+/**********************/
+
 router.get("/newtech", async (req, res) => {
   const result = await db.getAllTechs();
   logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
@@ -94,8 +108,47 @@ router.post("/newtech", project_upload.none(), async (req, res) => {
   return res.status(500).send("<h1>500 Internal Server Error</h1><p>Something is wrong on our end, sorry!</p>");
 });
 
+/**********************/
+
+// cookbook routes
+
+/**********************/
+
+router.get("/cookbook", async (req, res) => {
+  logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
+  const result = await db.getAllRecepies();
+  if (results === 500) {
+    return res.status(500).send("Unsuccess")
+  }
+  res.status(200).json(result);
+});
+
+router.post("/cookbook/new_recipe", cookbook_upload.single("recipe_image"), async (req, res) => {
+  logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
+  let body = req.body;
+  if (!req.file) {
+    return res.status(500).send("Something wrong with the file");
+  }
+  let img_src;
+  if (process.env.NODE_ENV === "dev") {
+    img_src = "http://localhost:3001/static/cookbook/" + req.file.filename;
+  } else {
+    img_src = "https://pauska.no/static/cookbook/" + req.file.filename;
+  }
+  const result = await db.newRecipe(body["recipeName"], body["recipeJson"], img_src);
+  if (result === 201) {
+    return res.status(201).send("Sucessfully created recipe");
+  }
+  return res.status(500).send("Internal Server Error");
+});
+
+
+/**********************/
 
 // deletion of data
+
+/**********************/
+
 router.delete("/tech/delete/:techID", async (req, res) => {
   logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
   const techID = parseInt(req.params["techID"]);
@@ -106,22 +159,22 @@ router.delete("/tech/delete/:techID", async (req, res) => {
   return res.status(500).send("Not deleted");
 });
 
-router.delete("/cookbook/delete/:recipeName", async (req, res) => {
+router.delete("/cookbook/delete/:recipeID", async (req, res) => {
   logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
-  const recipeName = req.params["recipeName"];
-  const result = await db.deleteRecipe(recipeName);
+  const recipeID = req.params["recipeID"];
+  const result = await db.deleteRecipe(recipeID);
   if (result === 200) {
     return res.status(200).send("Recipe deleted successfully");
   }
   return res.status(500).send("Not deleted");
 });
 
-router.delete("/cookbook/delete/:projectID", async (req, res) => {
+router.delete("/projects/delete/:projectID", async (req, res) => {
   logger.log("info", `HTTP ${res.statusCode} ${req.method} /api/admin${req.url}`);
   const projectID = parseInt(req.params["projectID"]);
   const result = await db.deleteProject(projectID)
   if (result === 200) {
-    return res.status(200).send("Recipe deleted successfully");
+    return res.status(200).send("Project deleted successfully");
   }
   return res.status(500).send("Not deleted");
 });
