492ddfda44a9ac461941fffc398443204c4c3efd
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-06-30 22:29:41 +0200

    refactor importing of functions

--- frontend/src/components/cookbook/recipe.js
+++ frontend/src/components/cookbook/recipe.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import parseToTitle from '../../utils';
+import {parseToTitle} from '../../utils';
 import '../../css/recipe.css';
 import {Back} from '../general';
 
