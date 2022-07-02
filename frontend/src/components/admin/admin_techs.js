6bf201fa7f9e675fd50b261f40f88fd22cc432a5
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-07-02 22:02:32 +0200

    add better styling to admin techs site

--- frontend/src/components/admin/admin_techs.js
+++ frontend/src/components/admin/admin_techs.js
@@ -36,9 +36,9 @@ class TechForm extends React.Component {
   render() {
     return (
       <div className="new-tech" onSubmit={this.handleSubmit}>
-        <h2>Add a new tech</h2>
+        <h2 className="mt--5">Add a new tech</h2>
         <form className="new-tech-form">
-          <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Name" className="form-input-text" required/>
+          <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Name" className="form-input" required/>
           <input type="submit" className="form-submit" value="Add" />
         </form>
       </div>
