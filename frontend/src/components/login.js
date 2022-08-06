44650d9ab2bb722b2648287939422c14cfffc967
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-08-06 13:15:00 +0200

    chore: style for login form

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -7,6 +7,7 @@ const cookies = new Cookies();
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,45 +16,50 @@ const Login = () => {
     form.append("username", username);
     form.append("password", password);
 
-    axios.post(process.env.REACT_APP_API_BASE_URL + "login", form, {
-      headers: {"Content-Type": "multipart/form-data"}
-    })
+    axios
+      .post(process.env.REACT_APP_API_BASE_URL + "login", form, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
       .then((res) => {
-        cookies.set("TOKEN", res.data.token, {path: "/"})
+        cookies.set("TOKEN", res.data.token, {
+          path: "/",
+          sameSite: "None",
+          secure: true,
+        });
         window.location.href = "/admin";
       })
       .catch((err) => {
-        err = new Error()
-      })
-  }
+        setError("Your username or password is wrong");
+        setPassword("");
+      });
+  };
 
   return (
     <div className="container-main">
-      <form onSubmit={handleSubmit}>
-        <input
-          className="form-input"
-          type="text"
-          value={username}
-          placeholder="Brukernavn"
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          className="form-input"
-          type="password"
-          value={password}
-          placeholder="Passord"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          className="form-submit"
-          type="submit"
-          value="Logg inn"
-        />
-      </form>
+      <div className="login-page">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            type="text"
+            value={username}
+            placeholder="Brukernavn"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            className="form-input"
+            type="password"
+            value={password}
+            placeholder="Passord"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input className="form-submit" type="submit" value="Logg inn" />
+        </form>
+        <p className="error">{error}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
