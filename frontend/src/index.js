fd19385e32cebe389f73cc29a66c036697c5dd38
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-08-06 13:14:48 +0200

    chore: redirect to admin if already logged in

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,38 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useParams
+  useParams,
+  Navigate,
 } from "react-router-dom";
-import Navbar from './components/navbar'
-import Home from './components/home';
-import Projects from './components/projects/projects';
-import Cookbook from './components/cookbook/cookbook';
-import Recipe from './components/cookbook/recipe';
-import Admin from './components/admin/admin';
-import './css/index.css';
-import './css/general.css';
-import Footer from './components/footer';
+import Navbar from "./components/navbar";
+import Home from "./components/home";
+import Projects from "./components/projects/projects";
+import Cookbook from "./components/cookbook/cookbook";
+import Recipe from "./components/cookbook/recipe";
+import Admin from "./components/admin/admin";
+import "./css/index.css";
+import "./css/general.css";
+import Footer from "./components/footer";
 import Login from "./components/login";
-import ProtectedRoutes from "./components/ProtectedRoutes"
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 const Page = (props) => {
   let { recipeName } = useParams();
   useEffect(() => {
     document.title = recipeName ? props.title + `/${recipeName}` : props.title;
-  }, [props.title]);
+  }, [props.title, recipeName]);
   return props.children;
-}
+};
 
 class Landing extends React.Component {
   render() {
+    const token = cookies.get("TOKEN");
     return (
       <Router>
         <Navbar />
@@ -43,16 +48,11 @@ class Landing extends React.Component {
           />
           <Route
             path="/admin/login"
-            element={
-              <Login />
-            }
+            element={token ? <Navigate to="/admin" replace /> : <Login />}
           />
-          <Route path="/admin/*" element={
-            <ProtectedRoutes />
-            }
-          />
-          <Route 
-            path="/projects" 
+          <Route path="/admin/*" element={<ProtectedRoutes />} />
+          <Route
+            path="/projects"
             element={
               <Page title="pauska/prosjekter">
                 <Projects />
@@ -85,4 +85,4 @@ class Landing extends React.Component {
 
 export default Page;
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Landing />)
+ReactDOM.createRoot(document.getElementById("root")).render(<Landing />);
